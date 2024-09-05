4a98eb92e3ed20245a473ed79e4c1ee38dc773d2
VidyaSinha <vidyasinha939@gmail.com>
2024-09-05 23:56:19 +0530

    DAY 1

---
+++ day1.java
@@ -0,0 +1,29 @@
+//today i solved the ques for Find Missing Observations in dice roll where we only know the mean of n+m rolls and dont know the mth rolls.
+
+
+class Solution {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+       int m= rolls.length;
+        int total= mean*(n+m);
+        int obssum=0;
+
+        for(int roll: rolls){
+            obssum+= roll;
+        }
+        int missingsum = total - obssum;
+        if (missingsum<n || missingsum>6*n){
+            return new int[0];
+        }
+        int q= missingsum/n;
+        int r=missingsum%n;
+        int[]res= new int[n];
+        for(int i=0; i<n; i++){
+            res[i]=q;
+        }
+        for(int i=0; i<r;i++){
+            res[i]++;
+
+        }
+        return res;
+    }
+}
